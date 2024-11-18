8bdd659c16b4ee3fa9a513b3e0abe14d4bb67021
Gokul Prathin <gokulprathin8@gmail.com>
2024-11-18 00:01:23 -0500

    added graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"golang-apis-kickstart/internal/config"
 	"golang-apis-kickstart/internal/controllers"
 	"golang-apis-kickstart/internal/database"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func init() {
@@ -23,13 +30,39 @@ func main() {
 	auth.POST("/register", controllers.CreateUser)
 	//auth.POST("/logout", Logout)
 
-	//v1.GET("/users", GetUsers)
-	//v1.POST("/users", CreateUser)
-	//v1.PUT("/users/:id", UpdateUser)
-	//v1.DELETE("/users/:id", DeleteUser)
+	srv := &http.Server{
+		Addr:    ":" + config.ServerPort,
+		Handler: router.Handler(),
+	}
 
-	err := router.Run(":" + config.ServerPort)
-	if err != nil {
-		panic(err)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	select {
+	case <-ctx.Done():
+		log.Println("Server gracefully shutdown")
 	}
 }
